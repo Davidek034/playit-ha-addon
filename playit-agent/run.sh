196fd66a3615f74edf54cc1c991a513a00fc68ec
Davidek034 <davi.luksik@gmail.com>
2026-07-23 14:56:20 +0200

    Update shebang in run.sh to use sh
    Changed the shebang from bash to sh for compatibility.

--- playit-agent/run.sh
+++ playit-agent/run.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "Starting Playit Agent..."
 
 if [ -z "$SECRET_KEY" ]; then
     echo "No secret key configured!"
-    echo "Open the addon options and add your Playit secret key."
     exit 1
 fi
 
